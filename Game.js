ceb8b625809aa439be881d589e1b6332117dcc04
judeallanhill <judeallanhill@icloud.com>
2023-09-22 16:34:09 -0700

    f

--- Game.js
+++ Game.js
@@ -1,15 +1,10 @@
+import {Rect} from "./RectUtils.js"
 let canvas = document.getElementById("canvas")
 let ctx = canvas.getContext("2d")
 let currentKey = new Map();
-let fullscreenElement = document.getElementById("fullscreen")
-fullscreenElement.addEventListener("click", openFullscreen); 
-function openFullscreen() {
-    if (canvas.requestFullscreen) {
-      canvas.requestFullscreen();
-    } else if (canvas.webkitRequestFullscreen) { /* Safari */
-      canvas.webkitRequestFullscreen();
-    } else if (canvas.msRequestFullscreen) { /* IE11 */
-      canvas.msRequestFullscreen();
+class Player {
+    constructor() {
+        this.bounds = new Rect(10,10,10,10,)
     }
 }
 

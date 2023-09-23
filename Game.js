a9c6b48e033ed14b31491b6688dc44c54ffe0269
judeallanhill <judeallanhill@icloud.com>
2023-09-22 17:10:37 -0700

    f

--- Game.js
+++ Game.js
@@ -4,7 +4,20 @@ let ctx = canvas.getContext("2d")
 let currentKey = new Map();
 class Player {
     constructor() {
-        this.bounds = new Rect(10,10,10,10,)
+        this.bounds = new Rect(10,10,10,10)
     }
 }
+function keyboardInit() {
+    window.addEventListener("keydown", function (event) {
+        currentKey.set(event.key, true)
+    });
+    window.addEventListener("keyup", function (event) {
+        currentKey.set(event.key, false);
+    });
+}
+function loop() {
 
+}
+function init() {
+
+}
